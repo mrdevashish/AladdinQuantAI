6cead60eab137797c9f588935ffe5c97ae5271b2
mrdevashish <71189261+mrdevashish@users.noreply.github.com>
2026-03-16 16:27:11 +0530

    Update api_server.py

--- api_server.py
+++ api_server.py
@@ -1,13 +1,35 @@
-from flask import Flask, jsonify
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
 import random
 
-app = Flask(__name__)
 
-@app.route("/signal")
-def signal():
-    return jsonify({
-        "symbol": "NIFTY",
-        "signal": random.choice(["BUY","SELL","HOLD"])
-    })
+PORT = 8000
 
-app.run(host="0.0.0.0", port=5000)
+
+class Handler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+
+        if self.path == "/signals":
+
+            data = {
+                "price": random.randint(23000, 24000),
+                "gamma": random.choice(["HIGH", "LOW"]),
+                "liquidity": random.choice(["BUY", "SELL"])
+            }
+
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+
+            self.wfile.write(json.dumps(data).encode())
+
+
+def run():
+    server = HTTPServer(("0.0.0.0", PORT), Handler)
+    print("API server running on port", PORT)
+    server.serve_forever()
+
+
+if __name__ == "__main__":
+    run()

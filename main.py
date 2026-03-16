3ad5f32e16cf6835051231b8ad430ac76ca6afd9
mrdevashish <71189261+mrdevashish@users.noreply.github.com>
2026-03-16 16:22:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,11 @@
 from kivy.app import App
 from kivy.uix.label import Label
-import threading
-
-from run_quant_pipeline import run_engines
 
 
 class AladdinApp(App):
 
     def build(self):
-
-        # run your quant engine in background
-        t = threading.Thread(target=run_engines)
-        t.daemon = True
-        t.start()
-
-        return Label(text="AladdinQuantAI Running")
+        return Label(text="AladdinQuantAI Mobile Dashboard")
 
 
 if __name__ == "__main__":
